990e78ce04203d83c7a5dc7f1102f996395a20a7
Daniel Ilies <iliesh.daniel@gmail.com>
2025-07-10 04:12:35 +0300

    fix cors

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://daniel-iliesh.github.io"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
